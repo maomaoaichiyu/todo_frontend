20d976f3bc7a36bf54a7a7981072398d74807366
Ying Wang <wangying220062@gmail.com>
2018-04-22 00:45:55 +0200

    display list of tasks
    adds vue store with a single module, tasks, to interact with the backend
    adds a component to display the list of tasks

---
+++ src/components/TaskList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="task in tasks" :key="task._id">{{ task.text }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex';
+
+export default {
+  name: 'TaskList',
+  mounted() {
+    this.getTasks();
+  },
+  computed: {
+    ...mapState({
+      tasks: state => state.tasks.tasks,
+    }),
+  },
+  methods: {
+    ...mapActions([
+      'getTasks',
+    ]),
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/store/modules/tasks.js
@@ -0,0 +1,21 @@
+import http from '@/utils/http';
+
+const BACKEND_URL = process.env.BACKEND_URL || 'http://127.0.0.1:8081';
+
+export default {
+  state: {
+    tasks: [],
+  },
+  mutations: {
+    setTasks(state, tasks) {
+      state.tasks = tasks;
+    },
+  },
+  actions: {
+    getTasks({ commit }) {
+      http.get(`${BACKEND_URL}/tasks`)
+        .then(response => JSON.parse(response))
+        .then(tasks => commit('setTasks', tasks));
+    },
+  },
+};
